2f8a83c81e65622661d69d58345a0f31a5acae52
Administrator <Administrator@MSDN-SPECIAL>
2015-05-11 11:12:48 +0900

    Launching..

---
+++ jse/src/cmm01/var/Var.java
@@ -0,0 +1,20 @@
+package cmm01.var;
+
+public class Var 
+{
+	public static void main(String[] args)
+	{	
+		int x;
+		
+		x= 1;
+		
+		int y;
+		
+		y = x + 1;
+		
+		System.out.println(y);
+		
+		System.out.println("x+1ÀÇ °ª y");
+	}
+	
+}
